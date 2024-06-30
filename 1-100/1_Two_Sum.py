959d17744bc1bbf7b7ad1b06a9263a682f9a6842
François "Kurikawa" Odin <francois@kurikawa.fr>
2024-06-30 23:39:01 +0200

    Problème 1 : Two Sum, sujet et signature de la fonction

---
+++ 1-100/1_Two_Sum.py
@@ -0,0 +1,17 @@
+"""
+== 1 - Two Sum ==
+
+Considérant une liste d'entiers 'l' et un entier 'x',
+Ecrire une fonction qui renvoit le couple (a,b),
+tel que a + b = x, a et b sont des éléments de 'l'
+
+Contraintes :
+1. 2 <= len(l) <= 10^4
+2. -10^9 <= l[i] <= 10^9
+3. -10^9 <= x <= 10^9
+
+Objectif facultatif : Ecrire une fonction dont la complexité
+est meilleure que quadratique.  
+"""
+def two_sum(values:list[int], target:int)->tuple[int, int]:
+    return []
